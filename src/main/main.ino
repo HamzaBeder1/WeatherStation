b4430b958f64800819518900a03abdcf21f516cf
HamzaBeder1 <hamzabeder0@gmail.com>
2024-07-30 21:54:03 -0500

    Finished I2C code for DS3231 RTC.

--- src/main/main.ino
+++ src/main/main.ino
@@ -1,6 +1,11 @@
 #include <Wire.h>
 
 #define BMP180_ADDR 119
+#define DS3231_ADDR 104
+
+#define BMP180_CTRL_MEAS 0xF4
+#define BMP180_OUT_MSB 0xF6
+
 void setup() {
   DDRD |= 0b00000010; //TX = output
   DDRD &= 0b11111110; //RX = input
@@ -9,13 +14,34 @@ void setup() {
   Wire.begin();
 }
 
+class DS3231{
+  private:
+    uint8_t second;
+    uint8_t minute;
+    uint8_t hour;
+    uint8_t day;
+    uint8_t date;
+    uint8_t month;
+    uint16_t year;
+
+  public:
+    void initDS3231(uint16_t, uint8_t *);
+    void getDataDS3231();
+    void hourModeSelect(int h);
+    void printDS3231();
+};
+
 void loop() {
-  /*getData();
-  delay(5000);
-  DDRD |= 0b00010000;*/
-  Serial.println(readRegister_BMP180(244));
-  delay(1000);
+  DS3231 x;
+  uint8_t arr[6] = {20, 34, 8, 3, 31, 7};
+  x.initDS3231(24, arr);
+  while(1){
+    x.getDataDS3231();
+    x.printDS3231();
+    delay(1000);
+  }
 }
+
 
 void DHT11init(){
   DDRD |= 0b00010000; //DHT11OUT = output
@@ -54,21 +80,87 @@ unsigned char * readByte(){
   return result;
 }
 
-unsigned char readRegister_BMP180(uint8_t addr){
-  Wire.beginTransmission(BMP180_ADDR); //begin communication with BMP180.
-  Wire.write(addr);
+unsigned char * readRegister_BMP180(unsigned char * data, int size){
+  Wire.beginTransmission(BMP180_ADDR); 
+  Wire.write(data[0]);
   Wire.endTransmission(BMP180_ADDR);
-  Wire.requestFrom(BMP180_ADDR, 1);
-  unsigned char a;
-  while(Wire.available()){
-     a = Wire.read();
+
+  Wire.requestFrom(BMP180_ADDR, size);
+  int i;
+  while(Wire.available() != size);
+  for(i = 0; i < size; i++)
+  { 
+     data[i] = Wire.read();
   }
-  return a;
+  return data;
 }
 
-void writeRegiste_BMP180(uint8_t addr, uint8_t data){
-  Wire.beginTransmission(BMP180_ADDR);
-  Wire.write(addr);
-  Wire.write(data);
+void writeRegister_BMP180(unsigned char * data, int size){
+  Wire.beginTransmission(BMP180_ADDR); 
+  Wire.write(data, size);
   Wire.endTransmission(BMP180_ADDR);
 }
+
+void DS3231::getDataDS3231(){
+  Wire.beginTransmission(DS3231_ADDR);
+  Wire.write(0x00);
+  Wire.endTransmission(DS3231_ADDR);
+
+  Wire.requestFrom(DS3231_ADDR, 7);
+  //while(Wire.available() != 7);
+
+  this->second = bcd2dec(Wire.read());
+  this->minute = bcd2dec(Wire.read());
+  uint8_t temp = Wire.read();
+  temp &= 0b10111111;
+  temp = bcd2dec(temp);
+  this->hour = temp; 
+  this->day = bcd2dec(Wire.read());
+  this->date=  bcd2dec(Wire.read());
+  this->month = bcd2dec(Wire.read());
+  this->year = 2000 + bcd2dec(Wire.read());
+
+}
+
+void DS3231:: initDS3231(uint16_t y, uint8_t * arr){
+  Wire.beginTransmission(DS3231_ADDR);
+  Wire.write(0x00); //go to first address for multibyte access.
+  for(int i = 0; i < 6; i++){
+    Wire.write(dec2bcd(arr[i]));
+  }
+  Wire.write(dec2bcd(y));
+  Wire.endTransmission(BMP180_ADDR);
+  hourModeSelect(12);
+}
+
+void DS3231::hourModeSelect(int h){
+  Wire.beginTransmission(DS3231_ADDR);
+  Wire.write(0x02);
+  Wire.endTransmission(DS3231_ADDR);
+  Wire.requestFrom(DS3231_ADDR, 1);
+  uint8_t reg = Wire.read();
+  if(h == 12)
+    reg |= 0b01000000;
+  else if(h == 24)
+    reg &= 0b10111111;
+  Wire.beginTransmission(DS3231_ADDR);
+  Wire.write(0x02);
+  Wire.write(reg);
+  Wire.endTransmission(DS3231_ADDR);
+}
+
+void DS3231::printDS3231(){
+  char buffer[50];
+  sprintf(buffer, "%d-%d-%d %d:%d:%d", year,month, date, hour, minute, second);
+  Serial.println(buffer);
+}
+
+uint8_t bcd2dec(uint8_t bcd){
+  return bcd/16*10 + bcd%16;
+}
+
+uint8_t dec2bcd(uint8_t dec){
+  return dec/10*16 + dec%10;
+}
+
+
